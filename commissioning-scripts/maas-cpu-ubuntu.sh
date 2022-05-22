79024afaf37b4c90adafd62ee6a432f423dd83cc
Aldo Rodríguez <43460704+aldorguez@users.noreply.github.com>
2022-05-21 18:16:41 -0700

    Update maas-cpu-ubuntu.sh

--- commissioning-scripts/maas-cpu-ubuntu.sh
+++ commissioning-scripts/maas-cpu-ubuntu.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# sudo vi /etc/ssh/sshd_config
-
 # Update
-#sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+#sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
 # NFS
-sudo apt install -y nfs-common
+sudo apt-get install -y nfs-common
 #  /share
 sudo mkdir /share
 sudo bash -c "echo -e '\n10.0.0.254:/share      /share   nfs   auto,nofail,noatime,nolock,intr,tcp,actimeo=1800    0     0\n' >> /etc/fstab"
@@ -18,11 +16,15 @@ sudo mount -a
 sudo ln -s /share/utils/modules/5.0.1/init/profile.sh /etc/profile.d/modules.sh
 
 # Infiniband
-sudo apt install -y rdma-core
+sudo apt-get install -y rdma-core
 
 # SLURM
-sudo apt install -y slurm-client slurmd
+sudo apt-get install -y slurm-client slurmd
 sudo cp -p /share/conf/slurm/{slurm.conf,cgroup.conf} /etc/slurm-llnl
+sudo chmod 644 /etc/slurm-llnl/{slurm.conf,cgroup.conf}
 
 #  Munge
 sudo cp -p /share/conf/munge/munge.key  /etc/munge
+sudo chmod --recursive 700 /etc/munge
+sudo chmod 400 /etc/munge/munge.key
+sudo chown --recursive munge:munge /etc/munge
