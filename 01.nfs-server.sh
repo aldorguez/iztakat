0b322bac534510badf0d22fdcba8996640ebc4fc
Aldo Rodríguez <43460704+aldorguez@users.noreply.github.com>
2022-06-08 18:37:07 -0700

    Update 01.nfs-server.sh

--- 01.nfs-server.sh
+++ 01.nfs-server.sh
@@ -5,10 +5,11 @@ sudo apt-get install -y nfs-common nfs-kernel-server
 sudo mkdir /share
 sudo mkdir /scratch
 
-echo -e "\n
+echo "
 /share 10.0.0.254(ro,sync,no_root_squash,no_subtree_check) 10.0.0.0/24(ro,async,no_subtree_check)
 /home  10.0.0.254(rw,async,no_root_squash) 10.0.0.0/24(rw,async,no_root_squash,no_subtree_check)
-/scratch  10.0.0.254(rw,async,no_root_squash) 10.0.0.0/24(rw,async,no_root_squash,no_subtree_check)\n" | \
+/scratch  10.0.0.254(rw,async,no_root_squash) 10.0.0.0/24(rw,async,no_root_squash,no_subtree_check)
+" | \
     sudo tee -a /etc/exports
 
 sudo systemctl restart nfs-server
